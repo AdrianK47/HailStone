e931ae6a201c98bd8977cd290b4cadd43fe65b3f
AdrianK47 <114188316+AdrianK47@users.noreply.github.com>
2024-10-08 15:50:27 -0400

    yes

--- HailStone.java
+++ HailStone.java
@@ -1,25 +1,31 @@
 import java.util.*;
 public class HailStone{
     public static void main(String[] args){
-        for (int i = 0; i < 200; i++){
-            
-    }
-
-    public static boolean isHailstoneNum(int s){
-        while (s == 0){
-            if (s != 1){
-                return false;
-                break;
-            }
-            if (s % 2 == 0){
-                s /= 2;
-                System.out.println(s); 
+        for (int i = 1; i < 201; i++){
+            if (isHailstoneNum(i)){
+                System.out.println("The number " + i + " is a hailstone number.");
             }
             else{
-                s = (s*3) + 1;
-                System.out.println(s);
+                System.out.println("The number " + i + " is not a hailstone number");
+        }
+    }
+}
+
+    public static boolean isHailstoneNum(int s){
+        boolean response = false;
+        int number = s;
+        while (!response){
+            if (number == 1){
+                response = true;
+                break;
+            }
+            else if (number % 2 == 0){
+                number = number/2;
+            }
+            else if (number % 2 == 1)  {
+                number = (number*3) + 1;
             }
         }
-        return true;
+        return response;
     }
 }
--- RandomWalk.java
+++ RandomWalk.java
@@ -1,4 +1,4 @@
-import java.util.*;
+/**import java.util.*;
 public class RandomWalk {
     double forward = 3.5;
     double backward = 3.5;
@@ -7,20 +7,17 @@ public class RandomWalk {
         int totalSteps = 0;
         for (int i = 0; i < 50; i++){
             double steps = Math.random();
-            System.out.println(steps);
             int numOfSteps = getNumOfSteps(steps);
-            System.out.println(numOfSteps);
             totalSteps += numOfSteps;
-            System.out.println(totalSteps);
         }
         int average = totalSteps / 50;
-        System.out.println("It took an average of " + average + "steps before taking a step off the bridge.");
+        System.out.println("It took an average of " + average + " steps before taking a step off the bridge.");
     }
     public static int getNumOfSteps(double s){
         double forward = 3.5;
         double backward = 3.5;
         int num = 0;
-        while (forward > 0.0 || backward > 0.0){
+        while (forward > 0.0 && backward > 0.0){
             if (s < 1 && s > 0){
                 backward -= 1.0;
                 num += 1;
@@ -29,10 +26,8 @@ public class RandomWalk {
                 num += 1;
                 forward -= 1.0;
             }
-            if (forward == 0 || backward == 0){
-                break;
-            }
         }
         return num;
     }
 }
+**/
