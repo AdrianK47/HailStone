02774da27cd1c7adac27d8bb14cf8f736f33303b
AdrianK47 <114188316+AdrianK47@users.noreply.github.com>
2024-10-07 21:07:39 -0400

    heheheheheh

--- HailStone.java
+++ HailStone.java
@@ -1,16 +1,25 @@
 import java.util.*;
 public class HailStone{
     public static void main(String[] args){
-        for (int i = 201; i <= 0; i--){
-            if (i % 2 == 0){
-                i /= 2;
-                System.out.println(i);
+        for (int i = 0; i < 200; i++){
+            
+    }
+
+    public static boolean isHailstoneNum(int s){
+        while (s == 0){
+            if (s != 1){
+                return false;
+                break;
+            }
+            if (s % 2 == 0){
+                s /= 2;
+                System.out.println(s); 
             }
             else{
-                i  = (i*3) + 1;
-                System.out.println(i);
+                s = (s*3) + 1;
+                System.out.println(s);
             }
-            System.out.println(i);
         }
+        return true;
     }
 }
--- RandomWalk.java
+++ RandomWalk.java
@@ -4,26 +4,35 @@ public class RandomWalk {
     double backward = 3.5;
     int num = 0;
     public static void main(String[] args){
-        for (int i = 0; i < 51; i++){
+        int totalSteps = 0;
+        for (int i = 0; i < 50; i++){
             double steps = Math.random();
-            getNumOfSteps(steps);
+            System.out.println(steps);
+            int numOfSteps = getNumOfSteps(steps);
+            System.out.println(numOfSteps);
+            totalSteps += numOfSteps;
+            System.out.println(totalSteps);
         }
+        int average = totalSteps / 50;
+        System.out.println("It took an average of " + average + "steps before taking a step off the bridge.");
     }
     public static int getNumOfSteps(double s){
         double forward = 3.5;
         double backward = 3.5;
         int num = 0;
-        while (forward >= 0 || backward >= 0){
-            if (s == 0){
-                backward -= 1;
+        while (forward > 0.0 || backward > 0.0){
+            if (s < 1 && s > 0){
+                backward -= 1.0;
                 num += 1;
             }
             else {
                 num += 1;
-                forward -= 1;
+                forward -= 1.0;
+            }
+            if (forward == 0 || backward == 0){
+                break;
+            }
         }
         return num;
-        
     }
-}
 }
